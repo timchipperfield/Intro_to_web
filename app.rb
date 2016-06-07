347ecf6089a0c83cbb1772bfb20870d565577511
timchipperfield <timchipperfield@gmail.com>
2016-06-07 12:32:55 +0100

    red line around kitty

--- app.rb
+++ app.rb
@@ -8,11 +8,10 @@ get '/' do
 end
 
 get '/cat' do
-	"<HEAD>
-			<TITLE> My Cat </TITLE>
-		</HEAD>
-		<BODY>
-			<img src='http://bit.ly/1eze8aE'>
-		</BODY>"
 
+  "<div style='border: 3px dashed red'>
+
+  <img src='http://bit.ly/1eze8aE'>
+
+  </div>"
 end
