6697c48daa739155c305e85e8f4d08308be63d89
riyapabari <riya.pabari@gmail.com>
2016-06-07 11:46:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Intro To Web
+
+Week 3 challenge
---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "hi!"
+end
+
+get '/cat' do
+	"<HEAD>
+			<TITLE> My Cat </TITLE>
+		</HEAD>
+		<BODY>
+			<img src='http://bit.ly/1eze8aE'>
+		</BODY>"
+end
